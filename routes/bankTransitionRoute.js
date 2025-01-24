c9234a80e00892782600f3df7d994e4769db9385
TanaratSudjai <650112418012@gmail.com>
2025-01-24 16:11:33 +0700

    post swager test

--- routes/bankTransitionRoute.js
+++ routes/bankTransitionRoute.js
@@ -2,10 +2,22 @@ const express = require("express");
 const router = express.Router();
 const bankControllers = require("../controllers/bankTransitionController");
 
+/**
+ * @swagger
+ * /api/bank_trantisionInsert:
+ *   post:
+ *     summary: Get example data
+ *     description: Retrieve example data from the server.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 router.post(
   "/bank_trantisionInsert",
   bankControllers.openAccountGroup_bankTransition
 );
+
+
 router.post("/bank_borrow", bankControllers.creditor_borrow_bankTransition);
 router.post("/bank_return", bankControllers.creditor_return_bankTransition);
 
