aa36107a1fc36af521aec03c5799061c8672d6e5
TanaratSudjai <650112418012@gmail.com>
2025-01-24 23:41:48 +0700

    deployed

--- controllers/swaggerController.js
+++ controllers/swaggerController.js
@@ -4,21 +4,20 @@ const auth = require("basic-auth");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const usernameswagger = process.env.USER_SWAGGERUI;
-const passwordswagger = process.env.PASS_SWAGGERUI;
-const basicAuthMiddleware = (req, res, next) => {
-  const user = auth(req);
-  if (user && user.name === usernameswagger && user.pass === passwordswagger) {
-    return next();
-  }
-  res.set("WWW-Authenticate", 'Basic realm="Enter your credentials"');
-  return res.status(401).send("Authentication required.");
-};
+// const usernameswagger = process.env.USER_SWAGGERUI;
+// const passwordswagger = process.env.PASS_SWAGGERUI;
+// const basicAuthMiddleware = (req, res, next) => {
+//   const user = auth(req);
+//   if (user && user.name === usernameswagger && user.pass === passwordswagger) {
+//     return next();
+//   }
+//   res.set("WWW-Authenticate", 'Basic realm="Enter your credentials"');
+//   return res.status(401).send("Authentication required.");
+// };
 
 const setupSwagger = (server) => {
   server.use(
     "/domain/api",
-    basicAuthMiddleware,
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec)
   );
