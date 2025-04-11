32f81a33ffa3cb3d45a1c804eeb1c5b754f9fe04
ZonixEz <149657519+SonicEz99@users.noreply.github.com>
2025-04-11 15:06:14 +0700

    get-dasktop_data

--- controllers/dataDasktopController.js
+++ controllers/dataDasktopController.js
@@ -19,27 +19,35 @@ exports.get_sumvalue_type = async (req, res) => {
 
   const query = `
     SELECT
-        account_type.account_type_sum, 
-        account_type.account_type_name, 
-        account_category.account_category_id, 
-        account_type.account_type_id, 
-        account_type.account_group_id, 
-        account_group.account_category_id
-    FROM
-        account_group
-        INNER JOIN  
-        account_category
+    account_type.account_type_sum, 
+    account_type.account_type_name, 
+    account_category.account_category_id, 
+    account_type.account_type_id, 
+    account_type.account_group_id, 
+    account_group.account_category_id
+FROM
+    account_group
+    INNER JOIN account_category
         ON account_group.account_category_id = account_category.account_category_id
-        INNER JOIN
-        account_type
+    INNER JOIN account_type
         ON account_type.account_group_id = account_group.account_group_id
-    WHERE 
-        account_group.account_user_id = ?
-    ORDER BY 
-        account_type.account_category_id ASC,
-        account_type.account_group_id ASC,
-        account_type.account_type_id ASC, 
-        account_type.account_type_name ASC
+WHERE 
+    account_group.account_user_id = ?
+ORDER BY 
+    CASE account_category.account_category_id
+        WHEN 1 THEN 1
+        WHEN 6 THEN 2
+        WHEN 7 THEN 3
+        WHEN 2 THEN 4
+        WHEN 3 THEN 5
+        WHEN 4 THEN 6
+        WHEN 5 THEN 7
+        ELSE 8
+    END,
+    account_type.account_group_id ASC,
+    account_type.account_type_id ASC, 
+    account_type.account_type_name ASC;
+
   `;
 
   try {
