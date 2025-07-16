912b15501cca0fb318b234b58132281ec2017bc6
TanaratSudjai <tanarat.dev18@gmail.com>
2025-07-16 16:09:09 +0700

    -controller

--- routes/AccountsReceivableAndBankRoute.js
+++ routes/AccountsReceivableAndBankRoute.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 const AccountsReceivableAndBankController = require("../controllers/AccountsReceivableAndBankController");
 
-router.put('/account_type_important_one_update/:account_group_id', AccountsReceivableAndBankController.MakeImportantToOne);
+router.put(
+  "/account_type_important_one_update/:account_group_id",
+  AccountsReceivableAndBankController.MakeImportantToOne
+);
 
-router.put('/account_type_important_zero_update/:account_group_id', AccountsReceivableAndBankController.MakeImportantToZero);
-
+router.put(
+  "/account_type_important_zero_update/:account_group_id",
+  AccountsReceivableAndBankController.MakeImportantToZero
+);
+router.post(
+  "/report_account",
+  AccountsReceivableAndBankController.ReportAccount
+);
 module.exports = router;
